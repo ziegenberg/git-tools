e5269a7d1e2aad87ece1c8e64dcb739051e5d6bc
Jan Schneider <jan@horde.org>
2017-10-20 16:25:33 +0200

    Revert "Revert "Revert "Make the callbacks package hook, and remove symlinks before upgrade and uninstall."""
    This reverts commit 63ee06e3e4c8404e89d00b2ff2f5b199eb2d4b43.

--- lib/Callbacks.php
+++ lib/Callbacks.php
@@ -18,6 +18,7 @@ use Composer\Installer\PackageEvent;
 
 class Callbacks
 {
+
     /**
      * Links the Horde_Role files needed to detect "Horde" as a valid role
      * when using PEAR via Composer.
@@ -35,18 +36,5 @@ class Callbacks
             dirname(__FILE__) . '/../vendor/pear-pear.horde.org/Horde_Role/PEAR/Installer/Role/Horde.xml',
             dirname(__FILE__) . '/../vendor/pear/pear/PEAR/Installer/Role/Horde.xml'
         );
-    }
-
-    /**
-     * Unlinks the Horde_Role files needed to detect "Horde" as a valid role
-     * when using PEAR via Composer.
-     *
-     * @param  Event  $event [description]
-     * @return [type]        [description]
-     */
-    public static function unlinkHordeRole(Event $event)
-    {
-        unlink(dirname(__FILE__) . '/../vendor/pear/pear/PEAR/Installer/Role/Horde.php');
-        unlink(dirname(__FILE__) . '/../vendor/pear/pear/PEAR/Installer/Role/Horde.xml');
     }
 }
